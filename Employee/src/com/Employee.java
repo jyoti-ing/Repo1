61fe55d7cdca45ec53f379550aab68cba87caaf8
jyoti-ing <71215887+jyoti-ing@users.noreply.github.com>
2025-05-02 11:49:25 +0530

    Update Employee.java

--- Employee/src/com/Employee.java
+++ Employee/src/com/Employee.java
@@ -4,4 +4,8 @@ public class Employee {
 public static void main(String[] args) {
 	System.out.println("This is a employee class");
 }
+
+	public void m2(){
+	System.out.println("m2-Employee");
+	}
 }
