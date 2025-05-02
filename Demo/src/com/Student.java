c2b99ed3848e7885899386a34bfe978221749fa1
jyoti-ing <jyotiingle23@gmail.com>
2025-05-02 11:44:21 +0530

    by jyoti

--- Demo/src/com/Student.java
+++ Demo/src/com/Student.java
@@ -4,6 +4,8 @@ public class Student {
 public static void main(String[] args) {
 	System.out.println("this is Student class");
        System.out.println("Student class");
+       System.out.println("Student");
+      
 	
  }
 public void m1() {
